3bbfd74326e9b30ac578a907cb5aa53c54597747
enea jorgo <eneajorgo1@gmail.com>
2024-12-12 14:44:50 +0100

    commit

---
+++ models/mart_faa_stats.sql/mart_route_stats.sql
@@ -0,0 +1,31 @@
+WITH routes AS (
+    SELECT origin
+           , dest
+           , count(*) AS n_flights_per_route
+           , count(DISTINCT tail_number) AS arr_tail
+           , count(DISTINCT airline) AS nunique_airlines
+           , avg(actual_elapsed_time) AS avg_elapsed_time
+           , avg(arr_delay) AS avg_delay
+           , max(arr_delay) AS max_arr_delay
+           , max(dep_delay) AS max_dep_delay
+           , min(arr_delay) AS min_arr_delay
+           , min(dep_delay) AS min_dep_delay
+           , sum(cancelled) AS cancelled_tot
+           , sum(diverted) AS diverted_tot
+    FROM {ref{"prep_flights"}}
+    GROUP BY origin, dest
+), routes_join_origin AS (
+    SELECT 
+     	a.city AS origin_city
+     	, a.city AS origin_country
+     	, a.name AS origin_name
+     	FROM routes r
+    JOIN {ref{"prep_airports"}}
+    ON r.origin = a.faa
+)
+SELECT *
+FROM routes_join_origin;
+JOIN {ref{"prep_airports"}}
+on rjo.dest = a.faa
+
+
